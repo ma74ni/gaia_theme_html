c3141edd97aeca248137d33515d4c6524baef2aa
Diego Paredes <diego.paredes@ppm.com.ec>
2022-09-06 20:11:43 -0500

    onboarding

--- js/app.js
+++ js/app.js
@@ -30,4 +30,50 @@ $(document).ready(function () {
       }
     });
   }
+  function showNavbar(toggleId, navId, coverId) {
+    const toggle = document.getElementById(toggleId),
+      nav = document.getElementById(navId),
+      cover = document.getElementById(coverId);
+
+    // Validate that all variables exist
+    if (toggle && nav) {
+      $(toggle).on('click', () => {
+        // show navbar
+        nav.classList.toggle('show');
+        cover.classList.toggle('active');
+      });
+    }
+  }
+  function closeNavbar(navId, coverId, btnId) {
+    const nav = document.getElementById(navId),
+      cover = document.getElementById(coverId);
+    if ((navId, coverId, btnId)) {
+      $(btnId).on('click', () => {
+        nav.classList.toggle('show');
+        cover.classList.toggle('active');
+      });
+    }
+  }
+  showNavbar('header-toggle', 'nav-bar', 'cover');
+  closeNavbar('nav-bar', 'cover', '#navbarClose');
+  closeNavbar('nav-bar', 'cover', '#cover');
+
+  //inputs
+  $('.custom-input-group input')
+    .focusin(function () {
+      $(this).parent().parent().addClass('selected');
+    })
+    .focusout(function () {
+      if ($(this).val() === '') {
+        $(this).parent().parent().removeClass('selected');
+      }
+    });
+  $('.custom-input-group button').on('click', () => {
+    const currentInput = $('.selected .custom-input-group input');
+    const typeInput = currentInput.prop('type');
+    typeInput === 'password'
+      ? currentInput.prop('type', 'text')
+      : currentInput.prop('type', 'password');
+  });
+  $('.content-input-group.verified').addClass('selected');
 });
